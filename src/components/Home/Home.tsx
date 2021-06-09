277a6bdf85c6fb1f95ba594eb6e3b64a1167a50c
DimBer31 <bernadine.dimitri@orange.fr>
2021-06-09 17:17:50 +0200

    NavBar added

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './Home.module.css';
+
+function Home(){
+
+    return(
+        <p className={styles.Home}>
+            Overwatch
+        </p>
+    );
+
+}
+
+export default Home;
